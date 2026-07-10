d55c5d7b334f0aae0244d9754ce96966d44fbcae
Cuyqo <saon0410@gmail.com>
2026-07-11 00:53:30 +0900

    feat(#1): 내 정보 조회 API 구현

--- src/main/java/com/hackathon/domain/member/service/AuthService.java
+++ src/main/java/com/hackathon/domain/member/service/AuthService.java
@@ -1,5 +1,6 @@
 package com.hackathon.domain.member.service;
 
+import com.hackathon.domain.member.dto.AuthDto;
 import com.hackathon.domain.member.dto.AuthDto.LoginRequest;
 import com.hackathon.domain.member.dto.AuthDto.SignUpRequest;
 import com.hackathon.domain.member.dto.AuthDto.TokenResponse;
@@ -50,5 +51,11 @@ public class AuthService {
 		String refreshToken = jwtTokenProvider.createRefreshToken(member.getId());
 
 		return new TokenResponse(accessToken, refreshToken);
+	}
+
+	public AuthDto.MemberInfoResponse getMyInfo(Long memberId) {
+		Member member = memberRepository.findById(memberId)
+				.orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
+		return new AuthDto.MemberInfoResponse(member.getId(), member.getLoginId(), member.getNickname(), member.getTotalScore());
 	}
 }
